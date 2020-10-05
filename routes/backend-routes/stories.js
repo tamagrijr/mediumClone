41befe30adc03e52b15a9d5089d22eba07104af6
Cynthia Spence <cynthiaspence7827@gmail.com>
2020-10-05 16:32:15 -0700

    created GET method for /api/stories/:id to get a single story by pk (id)

--- routes/backend-routes/stories.js
+++ routes/backend-routes/stories.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const { asyncHandler } = require('../../utils');
 const db = require('../../db/models');
-const { Story } = db;
+const { Story, Comment, Like } = db;
 
 const router = express();
 
 router.get('/api/stories/:id', asyncHandler(async (req, res) => {
-  const stories = Story.findByPk(req.params.id);
-  
+  const story = await Story.findByPk(req.params.id);
+  res.json({ story });
 }));
 
 module.exports = router;
