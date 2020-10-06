631382bb0647b0d6f0067526f179f0b7bb01a210
Cynthia Spence <cynthiaspence7827@gmail.com>
2020-10-06 10:08:03 -0700

    added get route for /api/comments/:userId to get all comments by a user

--- routes/backend-routes/comments.js
+++ routes/backend-routes/comments.js
@@ -14,6 +14,18 @@ const commentValidator = [
     .withMessage('Your comment must have a body')
 ];
 
+router.get(
+  '/:userId(\\d+)',
+  asyncHandler(async (req, res) => {
+    const userId = parseInt(req.params.userId);
+    const userComments = await Comment.findAll({
+      where: {
+        userId
+      }
+    });
 
+    res.json({ comments });
+  })
+);
 
 module.exports = router;
