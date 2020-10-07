fe76129c03c84b810f195a6226f1c619fba3a738
Cynthia Spence <cynthiaspence7827@gmail.com>
2020-10-07 11:10:19 -0700

    display splashtags on splash page

--- public/js/index.js
+++ public/js/index.js
@@ -1,24 +1,29 @@
-const errBtn = document.querySelector(".errorButton");
+// const errBtn = document.querySelector(".errorButton");
+const splashTagContainer = document.querySelector(".splash-tag-container");
 
-errBtn.addEventListener('click', (e) => {
-  e.preventDefault();
+window.addEventListener('DOMContentLoaded', async () => {
+  const storiesJson = await fetch('http://localhost:3000/api/stories/');
+  const stories = await storiesJson.json();
+  stories.stories.forEach(story => {
+    const splashTag = document.createElement('a');
+    const tagImg = document.createElement('img');
+    const tagTxt = document.createElement('span');
+    tagImg.setAttribute('src', 'favicon-32x32.png');
+    tagTxt.innerHTML = story.title;
+    splashTag.appendChild(tagImg);
+    splashTag.appendChild(tagTxt);
+    splashTagContainer.appendChild(splashTag);
+  });
+});
 
-
-
+// errBtn.addEventListener('click', (e) => {
+//   e.preventDefault();
     // res.status(500);
-
-
     //         // // storage access_token in localStorage:
     //         // localStorage.setItem("MEDIUM_ACCESS_TOKEN", token);
     //         // localStorage.setItem("MEDIUM_CURRENT_USER_ID", id);
     //         // redirect to home page to see all tweets:
 
-    window.location.href = "/error-test";
+    // window.location.href = "/error-test";
 
-});
-
-const splashTagContainer = querySelector(".splash-tag-container");
-
-document.addEventListener('DOMContentLoaded', () => {
-  const stories = await fetch('http://localhost:3000/api/')
-});
+// });
