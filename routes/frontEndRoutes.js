4107405912cd06ab9335fc770eeb3dd64bb53d6e
Cynthia Spence <cynthiaspence7827@gmail.com>
2020-10-07 22:36:43 -0700

    added titles to the page in the head of layout.pug and passed in for all routes in frontEndRoutes.js

--- routes/frontEndRoutes.js
+++ routes/frontEndRoutes.js
@@ -9,45 +9,45 @@ const csrfProtection = csrf({cookie: true});
 
 //actual splash page
 frontEndRouter.get("/splash", (req, res) => {
-  res.render('splash');
+  res.render('splash', { title: "MEDAYUM"});
 });
 //splash page
 frontEndRouter.get("/", (req, res) => {
-    res.render('index');
+    res.render('index', { title: "MEDAYUM" });
 });
 //sign up form
 frontEndRouter.get("/sign-up", csrfProtection, (req, res) => {
-    res.render('sign-up', { csrfToken: req.csrfToken()});
+    res.render('sign-up', { csrfToken: req.csrfToken(), title: "Sign Up" });
 });
 //log-in form
 frontEndRouter.get("/log-in", csrfProtection, (req, res) => {
-    res.render('log-in', { csrfToken: req.csrfToken() });
+    res.render('log-in', { csrfToken: req.csrfToken(), title: "Log In" });
 });
 //user profile
 frontEndRouter.get("/users", (req, res) => {
-    res.render('profile');
+    res.render('profile', { title: "Profile" });
 });
 //edit user profile form
 frontEndRouter.get("/users/:id(\\d+)/edit", csrfProtection, (req, res) => {
-    res.render('edit-profile', { csrfToken: req.csrfToken() });
+    res.render('edit-profile', { csrfToken: req.csrfToken(), title: "Edit Profile" });
 });
 //create new story form
 frontEndRouter.get("/create", csrfProtection, (req, res) => {
-    res.render('create', { csrfToken: req.csrfToken() });
+    res.render('create', { csrfToken: req.csrfToken(), title: "Create a Story" });
 });
 // display story by id
 frontEndRouter.get("/stories/:id(\\d+)", async (req, res) => {
     let storyInfo = await fetch.getAllStoryInfo(req);
     console.log(storyInfo);
-    res.render('story-layout', { storyInfo });
+    res.render('story-layout', { storyInfo, title: storyInfo.title });
 });
 //display story edit form
 frontEndRouter.get("/stories/:id(\\d+)/edit", csrfProtection, (req, res) => {
-    res.render('story-edit-layout', { csrfToken: req.csrfToken() });
+    res.render('story-edit-layout', { csrfToken: req.csrfToken(), title: "Edit Story" });
 });
 //display feed
 frontEndRouter.get("/feed", (req, res) => {
-    res.render('feed');
+    res.render('feed', { title: "My Feed" });
 });
 //throw error
 frontEndRouter.get("/error-test", (req, res, next) => {
