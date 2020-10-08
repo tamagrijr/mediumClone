860956edaf347fadae76c3c67ca2c8ce91d7e9c3
tamagri <tamagrijr@gmail.com>
2020-10-08 16:51:23 -0500

    maybe

--- public/js/index.js
+++ public/js/index.js
@@ -2,54 +2,34 @@ import { loggedIn } from "./utils.js";
 
 const logInScreen = document.querySelector('.loggedIn');
 const logOutScreen = document.querySelector('.loggedOut');
+const signInButton = document.querySelector('#signInButton');
+const signUpButton = document.querySelector('#signUpButton');
 const signInForm = document.querySelector('.signIn');
 const signUpForm = document.querySelector('.signUp');
 
 const demoLogin = document.querySelectorAll('.demo');
-// const logoutButton = document.querySelector('#logoutButton');
-// const signInButton = document.querySelector('#signInButton');
-// const signUpButton = document.querySelector('#signUpButton');
-// const profileButton = document.querySelector('#profileButton');
-// const writeStoryButton = document.querySelector('#createStoryButton')
-// const signInLi = document.querySelector('.signInButton');
-// const signUpLi = document.querySelector('.signUpButton');
-// const dropdown = document.querySelector('.dropdown')
 
 let logged = loggedIn();
 if (logged) {
   logInScreen.classList.remove('hidden');
-  // signInLi.classList.add('hidden');
-  // signUpLi.classList.add('hidden');
 } else {
   logOutScreen.classList.remove('hidden');
-  // dropdown.classList.add('hidden');
-}
 
-// signInButton.addEventListener('click', e => {
-//   e.preventDefault();
-//   logInScreen.classList.add('hidden');
-//   logOutScreen.classList.add('hidden');
-//   signUpForm.classList.add('hidden');
-//   signInForm.classList.remove('hidden');
-// })
-// signUpButton.addEventListener('click', e => {
-//   e.preventDefault();
-//   logInScreen.classList.add('hidden');
-//   logOutScreen.classList.add('hidden');
-//   signInForm.classList.add('hidden');
-//   signUpForm.classList.remove('hidden');
-// })
-// logoutButton.addEventListener('click', (e) => {
-//   localStorage.removeItem('MEDIUM_ACCESS_TOKEN');
-//   localStorage.removeItem('MEDIUM_CURRENT_USER_ID');
-//   window.location.href = "/";
-// })
-// profileButton.addEventListener('click', e => {
-//   window.location.href = '/profile'
-// })
-// writeStoryButton.addEventListener('click', e => {
-//   window.location.href = '/create'
-// })
+}
+signInButton.addEventListener('click', e => {
+  e.preventDefault();
+  logInScreen.classList.add('hidden');
+  logOutScreen.classList.add('hidden');
+  signUpForm.classList.add('hidden');
+  signInForm.classList.remove('hidden');
+})
+signUpButton.addEventListener('click', e => {
+  e.preventDefault();
+  logInScreen.classList.add('hidden');
+  logOutScreen.classList.add('hidden');
+  signInForm.classList.add('hidden');
+  signUpForm.classList.remove('hidden');
+})
 demoLogin.forEach(elem => {
   elem.addEventListener('click', async (e) => {
     e.preventDefault();
--- public/js/layout.js
+++ public/js/layout.js
@@ -1,8 +1,7 @@
 import { loggedIn } from './utils.js';
 
 const logoutButton = document.querySelector('#logoutButton');
-const signInButton = document.querySelector('#signInButton');
-const signUpButton = document.querySelector('#signUpButton');
+
 const profileButton = document.querySelector('#profileButton');
 const writeStoryButton = document.querySelector('#createStoryButton')
 const signInLi = document.querySelector('.signInButton');
@@ -16,21 +15,6 @@ if (logged) {
 } else {
   dropdown.classList.add('hidden');
 }
-
-signInButton.addEventListener('click', e => {
-  e.preventDefault();
-  logInScreen.classList.add('hidden');
-  logOutScreen.classList.add('hidden');
-  signUpForm.classList.add('hidden');
-  signInForm.classList.remove('hidden');
-})
-signUpButton.addEventListener('click', e => {
-  e.preventDefault();
-  logInScreen.classList.add('hidden');
-  logOutScreen.classList.add('hidden');
-  signInForm.classList.add('hidden');
-  signUpForm.classList.remove('hidden');
-})
 logoutButton.addEventListener('click', (e) => {
   localStorage.removeItem('MEDIUM_ACCESS_TOKEN');
   localStorage.removeItem('MEDIUM_CURRENT_USER_ID');
