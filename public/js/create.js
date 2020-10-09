2eec5a6c9fe58e4157605ec711bf8f6483ece7e6
tamagri <tamagrijr@gmail.com>
2020-10-09 12:04:30 -0500

    testing

--- public/js/create.js
+++ public/js/create.js
@@ -1,20 +1,47 @@
-// import {
-//     handleErrors
-// } from "./utils.js";
+import { handleErrors } from "./utils.js";
 
+const userId = window.localStorage.getItem('MEDIUM_CURRENT_USER_ID')
 // import{ db } from '../../db/models';
 // import { checkForUser } from "../../utils.js";
 // import { is } from "sequelize/types/lib/operators";
 // import { post } from "../../routes/frontEndRoutes.js";
 // import { stories } from '../../routes/backend-routes/stories.js'
 // const { Story, User } = db;
+
 const createStoryForm = document.querySelector(".create-story-form");
 // const createStoryButton = document.querySelector('.create-story-button');
 
 const form = document.querySelector('.create-story-form')
-form.addEventListener('submit', e => {
+form.addEventListener('submit', async e => {
     e.preventDefault();
-    alert('hello')
+    const formData = new FormData(form);
+    const title = formData.get("title");
+    const body = formData.get("body");
+    const obj = {title, body, userId};
+    try {
+        // ADD THIS ONCE VALIDATION IS IMPLEMENTED
+        const res = await fetch("/api/stories", {
+          method: "POST",
+          body: JSON.stringify(obj),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        if (!res.ok) {
+          throw res;
+        }
+        // const {
+        //   token,
+        //   user: { id },
+        // } = await res.json();
+        // storage access_token in localStorage:
+        // localStorage.setItem("MEDIUM_ACCESS_TOKEN", token);
+        // localStorage.setItem("MEDIUM_CURRENT_USER_ID", id);
+        // redirect to home page:
+        window.location.href = "/";
+      } catch (err) {
+        handleErrors(err);
+      }
 })
 
 // createStoryButton.addEventListener('click', (e) => {
