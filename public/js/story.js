401e73efd4bd96a9b47f1b875d381449dca0209f
Clay <crp2529@gmail.com>
2020-10-09 11:47:48 -0700

    connecting to server now, but getting handleValidationError 400, progress

--- public/js/story.js
+++ public/js/story.js
@@ -1,6 +1,6 @@
-// import {
-//     handleErrors
-// } from "./utils.js";
+import {
+    handleErrors
+} from "./utils.js";
 
 const { db } = require('../../db/models');
 // import { checkForUser } from "../../utils.js";
@@ -10,7 +10,8 @@ const { db } = require('../../db/models');
 const { Story, User } = db;
 const createStoryForm = document.querySelector(".create-story-form");
 // const createStoryButton = document.querySelector('.create-story-button');
-
+const fetch = require('node-fetch');
+const { handleErrors } = require('./utils');
 const form = document.querySelector('.create-story-form')
 // form.addEventListener('submit', e => {
 //     e.preventDefault();
@@ -39,24 +40,59 @@ createStoryForm.addEventListener("submit", async (e) => {
 
     const data = {
         title,
-        body,
-        authorId: localStorage.getItem("MEDIUM_CURRENT_USER_ID")
-        // createdAt: new Date(),
-        // updatedAt: new Date()
+        body
     };
+    data.authorId = localStorage.getItem("MEDIUM_CURRENT_USER_ID");
     // await attachStoryToUser(data.authorId)
-    console.log(data)
-      
+    // console.log(data.authorId)
+    try {
+        const newStory = await fetch('http://localhost:3000/api/stories', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json"
+            },
+        });
+        
+        if (!newStory.ok) {
+            throw newStory;
+        }
+    } catch (error) {
+        // handleErrors(error)
+        // console.error(error);
+    }
 
+    //  const res = await fetch("/api/users/token", {
+    //     method: "POST",
+    //     body: JSON.stringify(body),
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //   });
+    //   if (!res.ok) {
+    //     throw res;
+    //   }
+    //   const {
+    //     token,
+    //     user: { id },
+    //   } = await res.json();
+    //   // storage access_token in localStorage:
+    //   localStorage.setItem("MEDIUM_ACCESS_TOKEN", token);
+    //   localStorage.setItem("MEDIUM_CURRENT_USER_ID", id);
+    //   // redirect to home page:
+    //   window.location.href = "/";
+    // } catch (err) {
+    //   handleErrors(err);
+    // }
     // try {
         // const isValid = checkForUser(data.authorId);
         // console.log(isValid);
         // if (isValid) {
-            await Story.create({
-                title: story.title,
-                body: story.body,
-                authorId: story.authorId
-            })
+            // await Story.create({
+            //     title: story.title,
+            //     body: story.body,
+            //     authorId: story.authorId
+            // })
         //     window.location.href = '/';
         // }
     // } catch (error) {
